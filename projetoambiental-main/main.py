a8cc2e1e01acf38a4600411221496ed3e4789985
meioambienteprojeto1-creator <meioambienteprojeto1@gmail.com>
2026-01-10 20:40:48 -0300

    update
    *estilização do contato.html
    *adição do mapa do google maps no contato.html
    *sistema de busca adicionado ao campanha.html
    *renderização dinamica dos dados das campanhas em index.html e campanha.html
    *simplificação de algumas classes no style.css

--- projetoambiental-main/main.py
+++ projetoambiental-main/main.py
@@ -1,12 +1,36 @@
 import os
 
-from flask import Flask, send_file, url_for, render_template
+from flask import Flask, send_file, url_for, render_template, request
 
 app = Flask(__name__)
 
+campanhas = [
+    {
+        "id": 0,
+        "titulo": "Campanha de reciclagem do lixo",
+        "data": "09/01/2026",
+        "imagem": "imagens/reciclagem.png",
+        "descricao": "Piripiri - Recicle corretamente e transforme o lixo em recursos. Cada ato consciente ajuda a reduzir o impacto no meio ambiente."
+    },
+    {
+        "id": 1,
+        "titulo": "Campanha de consumo consciente de água",
+        "data": "09/01/2026",
+        "imagem": "imagens/agua.png",
+        "descricao": "Piripiri - Economize água! Pequenas atitudes no dia a dia garantem um futuro sustentável e preservam este recurso vital."
+    },
+    {
+        "id": 2,
+        "titulo": "Campanha para o plantio de árvores",
+        "data": "09/01/2026",
+        "imagem": "imagens/arvore.png",
+        "descricao": "Piripiri - Ação ambiental para plantio de árvores e conscientização ecológica."
+    }
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", campanhas=campanhas)
 def main():
     app.run(port=int(os.environ.get('PORT', 80)), debug=True)
 
@@ -14,5 +38,54 @@ def main():
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+
+
+
+@app.route('/favicon.ico')
+def favicon():
+    path = os.path.join(app.root_path, 'static', 'icons', 'favicon.ico')
+    return send_file(path, mimetype='image/vnd.microsoft.icon')
+
+
+
+@app.route("/campanha", methods=["GET"])
+def campanha():
+    query = request.args.get("q", "").lower()
+
+    if query:
+        resultados = [
+            c for c in campanhas
+            if query in c["titulo"].lower() or query in c["descricao"].lower()
+        ]
+    else:
+        resultados = campanhas
+
+    return render_template("campanha.html", resultados=resultados, query=query)
+
+
+@app.route("/descricao/<int:id>")
+def descricao(id):
+    if 0 <= id < len(campanhas):
+        campanha_selecionada = campanhas[id]
+    else:
+        campanha_selecionada = None
+
+    return render_template("descricao.html", campanha=campanha_selecionada)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     main()
